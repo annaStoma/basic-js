addd652817d77e48010c02797d3846aa8b4d9735
Hanna Stoma <annstoma9@gmail.com>
2020-02-29 19:18:36 +0300

    fix: implement correct decision of vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,108 +4,45 @@ class VigenereCipheringMachine {
     }
     encrypt(message, key) {
         if (!message || !key) throw new Error();
+        let result = new String();
+        let eng = new Array();
         message = message.toUpperCase();
         key = key.toUpperCase();
-        let result = '';
-        let repeatPass = '';
-
-        for (let i = 0; i < message.length; i++) {
-            for (let i = 0; i < message.length; i++) {
-                repeatPass += key;
-            }
+        for (let i = 65; i < 91; i++) {
+            eng.push(String.fromCharCode(i));
         }
-        repeatPass = repeatPass.substr(0, message.length).split('');
-        for (let i = 0; i < repeatPass.length; i++) {
-            if (message[i] == " ") {
-                repeatPass.splice(i, 0, " ");
+        // eng = eng.join('');
+        for (let i = 0, k = 0; i < message.length; i++, k++) {
+            if (eng.includes(message[i])) {
+                let index = (eng.indexOf(message[i]) + eng.indexOf(key.charAt(k % key.length))) % eng.length;
+                result += eng[index];
+            } else {
+                k = k - 1;
+                result += message[i];
             }
-        }
-        repeatPass = repeatPass.join('').substr(0, message.length)
-        let matrix = [];
-        let k = 64;
-        let c = 65;
-        let maxLeter = 90;
-        for (let i = 0; i < 26; i++) {
-            matrix[i] = [];
-            k++;
-            c = 65;
-            for (let j = k; j <= maxLeter; j++) {
-                matrix[i].push(String.fromCharCode(j));
-            }
-            let h = 65;
-            if (matrix[i].length < 26) {
-                do {
-                    matrix[i].push(String.fromCharCode(h));
-                    h++;
-                }
-                while (matrix[i].length < 26)
-            }
-        }
-        let index = 0;
-        for (let i = 0; i < message.length; i++) {
-            if (matrix[0].includes(message[i])) {
-                index = matrix[0].indexOf(message[i]);
-                matrix.forEach(string => {
-                    if (string[0] == repeatPass[i]) {
-                        result += string[index];
-                    }
-                })
-            } else result += message[i];
         }
         return (this.reverse === false) ? result.split('').reverse().join('') : result;
     }
 
     decrypt(message, key) {
         if (!message || !key) throw new Error();
+        let result = new String();
+        let eng = new Array();
         message = message.toUpperCase();
         key = key.toUpperCase();
-        let result = '';
-        let repeatPass = '';
-        for (let i = 0; i < message.length; i++) {
-            for (let i = 0; i < message.length; i++) {
-                repeatPass += key;
-            }
-        }
-        repeatPass = repeatPass.substr(0, message.length).split('');
-        for (let i = 0; i < repeatPass.length; i++) {
-            if (message[i] == " ") {
-                repeatPass.splice(i, 0, " ");
-            }
-        }
-        repeatPass = repeatPass.join('').substr(0, message.length)
-        let matrix = [];
-        let k = 64;
-        let c = 65;
-        let maxLeter = 90;
-        for (let i = 0; i < 26; i++) {
-            matrix[i] = [];
-            k++;
-            c = 65;
-            for (let j = k; j <= maxLeter; j++) {
-                matrix[i].push(String.fromCharCode(j));
-            }
-            let h = 65;
-            if (matrix[i].length < 26) {
-                do {
-                    matrix[i].push(String.fromCharCode(h));
-                    h++;
-                }
-                while (matrix[i].length < 26)
-            }
+        for (let i = 65; i < 91; i++) {
+            eng.push(String.fromCharCode(i));
         }
 
-        let index = 0;
-        for (let i = 0; i < repeatPass.length; i++) {
-            if (matrix[0].includes(message[i])) {
-                matrix.forEach((string) => {
-                    if (string[0] == repeatPass[i]) {
-                        index = string.indexOf(message[i]);
-                        result += matrix[0][index]
-                    }
-                })
-            } else result += message[i];
+        for (let i = 0, k = 0; i < message.length; i++, k++) {
+            if (eng.includes(message[i])) {
+                let index = ((eng.indexOf(message[i]) + eng.length) - eng.indexOf(key.charAt(k % key.length))) % eng.length;
+                result += eng[index];
+            } else {
+                k = k - 1;
+                result += message[i];
+            }
         }
-
         return (this.reverse === false) ? result.split('').reverse().join('') : result;
     }
 }
